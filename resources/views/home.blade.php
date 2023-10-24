a3dda87ead980ce28e29d429ae0b926aa3c1a1b5
fajriaroyan <fajriaroyan12@gmail.com>
2023-10-24 18:49:50 +0700

    pertama push

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,13 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="{{asset('img/jpg.webp')}}" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="{{asset('img/jpg1.webp')}}" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="..." class="d-block w-100" alt="...">
+        <img src="{{asset('img/jpg2.webp')}}" class="d-block w-100" alt="...">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
